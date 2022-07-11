3818eec80fdb5b1604ced29884bb246fef34c161
aliiev <aliiev.akhtem@gmail.com>
2022-07-11 23:03:43 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [ './src/**/*.js' ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': [ 'Finlandica', defaultTheme.fontFamily.sans ],
+        'body': [ 'Andika', defaultTheme.fontFamily.serif ]
+      }
+    },
+  },
+  plugins: [],
+}
